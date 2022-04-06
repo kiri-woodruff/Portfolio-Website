452e2e92430313814a48bc1f348f0f5658af375d
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-04-06 15:25:44 -0400

    Basic Projects Page Styling

--- src/Projects-Page/ProjectsPage.tsx
+++ src/Projects-Page/ProjectsPage.tsx
@@ -1,11 +1,26 @@
 import './ProjectsPageStyling.css';
 import {Link} from "react-router-dom";
+import AudioStoriesAboutPhoto from '../Assets/AudioStories-About.png'
 
 const ProjectsPage = () => {
     return (
         <div className="projects-page">
-            <Link to={'/projects/audio-stories'}>Audio Stories</Link>
-            <Link to={'/projects/wiki-women'}>Wiki Women</Link>
+            <h1>Projects</h1>
+            <div className='projects-page-projects'>
+                <div className='project'>
+                    <div className='background-color-box'>
+                        <Link to={'/projects/audio-stories'}><img src={AudioStoriesAboutPhoto} height='200px' width='20%'/></Link>
+                        <Link to={'/projects/audio-stories'} className='projects-page-buttons'>Audio Stories</Link>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='background-color-box'>
+                        <Link to={'/projects/wiki-women'}><img src={AudioStoriesAboutPhoto} height='200px' width='20%'/></Link>
+                        <Link to={'/projects/wiki-women'} className='projects-page-buttons'>Wiki Women</Link>
+                    </div>
+                </div>
+
+            </div>
         </div>
     )
 }
