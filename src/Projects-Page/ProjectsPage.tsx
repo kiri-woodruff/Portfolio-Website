14b8f8f02d0abd4f25c444fbb848da8493c76c44
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-04-09 17:05:31 -0400

    Added Wiki Women photo and a third project

--- src/Projects-Page/ProjectsPage.tsx
+++ src/Projects-Page/ProjectsPage.tsx
@@ -1,6 +1,7 @@
 import './ProjectsPageStyling.css';
 import {Link} from "react-router-dom";
 import AudioStoriesAboutPhoto from '../Assets/AudioStories-About.png'
+import WikiWomenHomePhoto from '../Assets/WikiWomenHome.png'
 
 const ProjectsPage = () => {
     return (
@@ -9,14 +10,20 @@ const ProjectsPage = () => {
             <div className='projects-page-projects'>
                 <div className='project'>
                     <div className='background-color-box'>
-                        <Link to={'/projects/audio-stories'}><img src={AudioStoriesAboutPhoto} height='200px' width='20%'/></Link>
+                        <Link to={'/projects/audio-stories'}><img className='audio-stories-home-photo' src={AudioStoriesAboutPhoto} height='200px' width='20%'/></Link>
                         <Link to={'/projects/audio-stories'} className='projects-page-buttons'>Audio Stories</Link>
                     </div>
                 </div>
                 <div className='project'>
                     <div className='background-color-box'>
-                        <Link to={'/projects/wiki-women'}><img src={AudioStoriesAboutPhoto} height='200px' width='20%'/></Link>
+                        <Link to={'/projects/wiki-women'}><img className='wiki-women-home-photo' src={WikiWomenHomePhoto} height='200px' width='20%'/></Link>
                         <Link to={'/projects/wiki-women'} className='projects-page-buttons'>Wiki Women</Link>
+                    </div>
+                </div>
+                <div className='project'>
+                    <div className='background-color-box'>
+                        <Link to={'/projects/'}><img src={AudioStoriesAboutPhoto} height='200px' width='20%'/></Link>
+                        <Link to={'/projects/'} className='projects-page-buttons'>Third Project</Link>
                     </div>
                 </div>
 
