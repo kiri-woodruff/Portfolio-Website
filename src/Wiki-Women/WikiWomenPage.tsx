02f9b4d504e2a84d6357ba503fa75a7bdeff284e
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-05-10 11:18:24 -0400

    Wiki Woman projects page content

--- src/Wiki-Women/WikiWomenPage.tsx
+++ src/Wiki-Women/WikiWomenPage.tsx
@@ -1,4 +1,6 @@
 import './WikiWomenPageStyling.scss'
+import WikiWomenHomePhoto from '../Assets/WikiWomenHome.png'
+import WikiWomenInfoPagePhoto from '../Assets/WikiWomenInfoPage.png'
 
 const WikiWomenPage = () => {
     return (
@@ -9,10 +11,34 @@ const WikiWomenPage = () => {
                     Tools Used
                 </h2>
                 <ul>
-                    <li>Media Wiki API</li>
+                    <li>Wiki Data API</li>
                     <li>Java</li>
                     <li>Java FX</li>
                 </ul>
+                <p>
+                    <h3>Purpose</h3>
+                    For a class project, the other women in the class and I realized that women in STEM do not get as much recognition as their male peers.
+                    We also realized that in our curriculum we are not taught about women as much as we are taught about men. To address these problems
+                    we decided to make Wiki Women.
+                </p>
+
+                <p>
+                    <h3>What It Is</h3>
+                    Wiki Women is an application that displays 16 random women in any field of STEM. The application initially displays their name and
+                    photo (if one is available). The information is pulled from Wiki Data using the Wiki Data API and when you click on a woman it brings
+                    you to her information page. The information page displays her name, year of birth, occupation(s) and if applicable, photo,
+                    year of death, awards, and thesis.
+                </p>
+
+                <img src={WikiWomenHomePhoto}/>
+                <img src={WikiWomenInfoPagePhoto}/>
+
+                <p>
+                    <h3>What I Did</h3>
+                    I was responsible for making the UI. In order to properly display the information, I had to research how move between scenes in JavaFX
+                    and how to use a GridPane. I made the initial search engine where it worked when searching for the woman's
+                    Wiki Data ID and the information page that displays the woman's information.
+                </p>
             </div>
         </div>
     )
