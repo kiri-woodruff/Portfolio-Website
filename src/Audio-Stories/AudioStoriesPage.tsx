69f63b142eb2db3e5b10406ce0fcaafc8c432ada
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-05-10 16:29:16 -0400

    Audio stories content added, media styling

--- src/Audio-Stories/AudioStoriesPage.tsx
+++ src/Audio-Stories/AudioStoriesPage.tsx
@@ -22,7 +22,8 @@ const AudioStoriesPage = () => {
             </div>
             <p>
                 <h3>Purpose</h3>
-                Small Town * Big Art wanted a platform for the people of Maui to be able to share and record their stories regarding their culture and heritage.
+                Small Town * Big Art wanted a platform for the people of Maui to be able to share and record their
+                stories regarding their culture and heritage.
             </p>
             <div className='images'>
                 <img src={AudioStoriesTopics}/>
@@ -32,13 +33,23 @@ const AudioStoriesPage = () => {
             </div>
             <p>
                 <h3>What It Is</h3>
-                There are QR codes placed around Maui that will lead to one of the seven topics: Diversity, Legacy, Tradition, Protection, Hope, Kuleana (Responsibility), or Belonging.
-                On the topics page there are three different routes the user could take: Talk, Go, or Create. Talk is a way for the user to look through prompts about what story to share.
-                Go features different places that the user can go to, learn about, or share a story about. Create is where the user can see other stories submitted by other uses and be
-                inspired to submit a story of their own. There are three different ways to submit a story: the user can record an audio story, take a photo or video, or write a story.
+                There are QR codes placed around Maui that will lead to one of the seven topics: Diversity, Legacy,
+                Tradition, Protection, Hope, Kuleana (Responsibility), or Belonging.
+                On the topics page there are three different routes the user could take: Talk, Go, or Create. Talk is a
+                way for the user to look through prompts about what story to share.
+                Go features different places that the user can go to, learn about, or share a story about. Create is
+                where the user can see other stories submitted by other uses and be
+                inspired to submit a story of their own. There are three different ways to submit a story: the user can
+                record an audio story, take a photo or video, or write a story.
             </p>
-            <p>
+            <p className='whatIDid'>
                 <h3>What I Did</h3>
+                I was responsible for the functionality of swiping through the prompts and being able to record an audio
+                story by utilizing the Media Recorder API.
+                In addition to functionality, I was tasked with styling the about page that displays the topics, the
+                pages that display the prompts and stories with
+                different media types, the continue the story page that displays the stories, the page with the different ways of
+                submitting a story, and the recording button.
             </p>
         </div>
     )
