87ffe765fb128bb6512b1e590fa240ba0481765b
Kiri Woodruff <kdwoodruff@bsu.edu>
2022-08-09 12:26:52 -0400

    edit contact me page

--- src/Pages/Contact-Me/ContactMePage.tsx
+++ src/Pages/Contact-Me/ContactMePage.tsx
@@ -3,22 +3,29 @@ import github from '../../Assets/github-logo.png';
 import email from '../../Assets/email.png';
 import linkedin from '../../Assets/linkedin-logo.png'
 
-function ContactMePage(){
-    return(
+function ContactMePage() {
+    return (
         <div className='contact-me'>
             <div>
-                Contact Me!
+                <h1>Contact Me!</h1>
             </div>
-            <div className='footer-icons'>
-                <a href={'https://github.com/kiri-woodruff'} target={"_blank"}>
-                    <img src={github} alt='GitHub'/>
-                </a>
-                <a href={"mailto: kiri@woodruffs.com"} target={"_blank"}>
-                    <img src={email}/>
-                </a>
-                <a href={'https://www.linkedin.com/in/kiri-woodruff/'} target={"_blank"}>
-                    <img src={linkedin}/>
-                </a>
+            <div className='positioning'>
+                <div className='footer-icons'>
+                    <div>
+                        <a href={"mailto: kiri@woodruffs.com"} target={"_blank"}>
+                            <p>kiri@woodruffs.com</p>
+                        </a>
+                    </div>
+                    <div className='links'>
+                        <a href={'https://github.com/kiri-woodruff'} target={"_blank"}>
+                            <img src={github} alt='GitHub'/>
+                        </a>
+
+                        <a href={'https://www.linkedin.com/in/kiri-woodruff/'} target={"_blank"}>
+                            <img src={linkedin}/>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     )
