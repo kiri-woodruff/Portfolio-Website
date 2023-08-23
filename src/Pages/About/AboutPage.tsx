b1ce85e4d19df7c1b72e2531034acff6763253aa
kiri-woodruff <kiri.woodruff@gmail.com>
2023-08-23 10:19:22 -0400

    Change about me page content

--- src/Pages/About/AboutPage.tsx
+++ src/Pages/About/AboutPage.tsx
@@ -5,7 +5,8 @@ import {motion} from "framer-motion";
 
 const AboutPage = () => {
     return (
-        <motion.div className='about-page' initial={{opacity:0, x:100}} animate={{opacity:1, x:0}} exit={{opacity:0, x: -100}} transition={{duration: 0.5}} >
+        <motion.div className='about-page' initial={{opacity: 0, x: 100}} animate={{opacity: 1, x: 0}}
+                    exit={{opacity: 0, x: -100}} transition={{duration: 0.5}}>
             <HelmetProvider>
                 <Helmet>
                     <title>About Me</title>
@@ -18,14 +19,16 @@ const AboutPage = () => {
                 </div>
                 <div className='about-me'>
                     <p>
-                    I'm a Junior at Ball State University majoring in Computer Science with a minor in Sociology.
+                        I'm a Senior at Ball State University majoring in Computer Science with a minor in Sociology.
                     </p>
                     <p>
-                    I work at the Digital Corps at Ball State, where we work in interdisciplinary teams to create web
-                    applications and other digital products for clients.
+                        I work at the Digital Corps at Ball State, where we work in interdisciplinary teams to create
+                        web
+                        applications and other digital products for clients.
                     </p>
                     <p className='what-i-am'>
-                    At the Digital Corps, I am currently a Development Specialist and a Junior Project Manager.
+                        At the Digital Corps, I am currently a Development Master and a Junior Project Manager. What I
+                        do is be a leader and spokesperson of the Corps and I aid in the management of projects.
                     </p>
                 </div>
             </div>
