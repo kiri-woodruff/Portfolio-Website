67edb67d638ffc9f263e3eb9b479a434a385eaf5
kiri-woodruff <kiri.woodruff@com>
2024-02-20 16:02:40 -0500

    Add content to tlz page

--- src/Pages/TLZ-Website/TLZWebsitePage.tsx
+++ src/Pages/TLZ-Website/TLZWebsitePage.tsx
@@ -33,6 +33,9 @@ function TLZWebsitePage() {
                     <li>CSS/SCSS</li>
                     <li>React JS/TS</li>
                     <li>Keyframes</li>
+                    <li>AdonisTS</li>
+                    <li>Docker</li>
+                    <li>AWS</li>
                 </ul>
             </div>
             <div>
@@ -42,11 +45,12 @@ function TLZWebsitePage() {
             </div>
             <div>
                 <h3>What It Is</h3>
-                <p><a href='https://tlz.apsoprojects.org/' target="_blank" className='link-out'>The Loading Zone</a> is a program that is created by the Digital Corps as a means of teaching children
+                <p><a href='https://tlz.apsoprojects.org/' target="_blank" className='link-out'>The Loading Zone</a> is
+                    a program that is created by the Digital Corps as a means of teaching children
                     about different topics. </p>
             </div>
             <div className='images'>
-                <Carousel images={[Login, GridView, ListView, VideoPlayer]} />
+                <Carousel images={[Login, GridView, ListView, VideoPlayer]}/>
             </div>
             <div>
                 <h3>What I Did</h3>
@@ -57,6 +61,11 @@ function TLZWebsitePage() {
                     desktop and 20 on mobile. I created the layout of displaying videos in the grid and list views and
                     used a dialog box to display a video player iframe to play the video after clicking on the video
                     thumbnail. I made the login page that directed to where PBS employees could download videos. </p>
+                <p>I edited the backend to accommodate the change in the api that we were getting the employees from.
+                    The api had changed so I had to make a new method in the controller to get the employees that worked
+                    at the Digital Corps after TLZ started in the Fall of 2017. I had to write new migrations to update
+                    the categories enum to be in alphabetical order and then include an orderBy in the categories query to order them
+                    alphabetically and put Other at the end.</p>
             </div>
             <div className='bottom-links'>
                 <div className='left-arrow'>
