67a3d663ae84ae83ccfaa7a15867552a40214e6c
kiriwoodruff <kiri.woodruff@gmail.com>
2024-06-13 15:53:30 -0400

    update about me

--- src/Pages/About/AboutPage.tsx
+++ src/Pages/About/AboutPage.tsx
@@ -19,17 +19,18 @@ const AboutPage = () => {
                 </div>
                 <div className='about-me'>
                     <p>
-                        I'm a Senior at Ball State University majoring in Computer Science with a minor in Sociology.
+                        I'm a recent graduate of Ball State University having earned a Bachelor's of Science with a
+                        major in Computer Science and a minor in Sociology.
                     </p>
                     <p>
-                        I work at the Digital Corps at Ball State, where we work in interdisciplinary teams to create
-                        web applications and other digital products for clients.
+                        I am a very social person looking for a social environment that offers collaboration and growth,
+                        and I am looking for new opportunities wherever that may take me!
                     </p>
                     <p className='what-i-am'>
-                        I am about to graduate from college and am extremely passionate about frontend web development
-                        with a specialty in frontend styling using CSS and SCSS. I am also a very social person and have
-                        used the skills in a personal and professional manner with relationship building and public
-                        speaking respectively.
+                        I am extremely passionate about frontend development having worked with primarily React.js,
+                        TypeScript, and WordPress and I have a specialty in SCSS/CSS styling. I also have work experience in
+                        backend development working with Adonis.js,
+                        Docker, and AWS and I am open to learning even more!
                     </p>
                 </div>
             </div>
